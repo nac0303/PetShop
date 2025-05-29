8ac644a55c3373a86ee5e10eb80673d29071b9f0
Steicy <85196547+steicysantos@users.noreply.github.com>
2025-05-29 08:14:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,17 @@
 $('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 5,
-    nav: true,
-    navText: [
-      "<i class='fa fa-caret-left'></i>",
-      "<i class='fa fa-caret-right'></i>"
-    ],
-    autoplayHoverPause: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      600: {
-        items: 2
-      },
-      1000: {
-        items: 3
-      },
-      1500:{
-        items:4
-      }
-
-    }
-  })
+  loop: true,
+  margin: 5,
+  nav: true,       // Ativa as setas
+  dots: false,     // Desativa os pontos (bolinhas)
+  navText: [
+    "<i class='fa fa-chevron-left'></i>",   // seta esquerda
+    "<i class='fa fa-chevron-right'></i>"   // seta direita
+  ],
+  autoplayHoverPause: true,
+  responsive: {
+    0: { items: 1 },
+    600: { items: 2 },
+    1000: { items: 3 },
+    1500: { items: 4 }
+  }
+});
